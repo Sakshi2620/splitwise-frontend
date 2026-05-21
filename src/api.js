30a639dbc68b79058b09cbe5e89b7cff79df9fb8
sakshi <sakshijagtap882249@gmail.com>
2026-05-21 17:52:50 +0530

    Updated production backend URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/api',
+  
+  baseURL: process.env.REACT_APP_API_URL || 'https://splitwise-backend-j4uh.onrender.com', 
   headers: { 'Content-Type': 'application/json' },
 });
 
